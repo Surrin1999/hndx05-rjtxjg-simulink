d8bda24e27761329362b22c742feab7f9111348c
“Surrin1999 <605308631@qq.com>
2021-12-08 17:05:56 +0800

    整合完了所有模块

---
+++ read_self.py
@@ -0,0 +1,10 @@
+"""
+Author: Surrin1999
+Date: 2021-12-08
+"""
+import pandas as pd
+
+
+def read(id):
+    self_family_data = pd.read_csv('self_family.csv')
+    return float(self_family_data.loc[id - 1, 'self'])
